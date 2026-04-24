c045fa4b969aba0290fe72023c82b2d26040423f
bgnarsipumum-gif <bgnarsipumum@gmail.com>
2026-04-24 20:06:00 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'kip-v1';
+const assets = ['./index.html'];
+
+// Proses Install
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Proses Fetch (Menampilkan konten)
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
